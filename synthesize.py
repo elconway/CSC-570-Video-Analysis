799717c9bf16b3fb847ffb35afec73ebb67de3aa
elconway <elconway@calpoly.edu>
2023-05-04 01:18:29 -0700

    synthesize.py improved

--- synthesize.py
+++ synthesize.py
@@ -7,13 +7,25 @@ def synthesize_data(filename1, filename2):
     emotion_data = emotion_data[1:]
     f1.close()
     f2.close()
+
+    # Parse eye tracking data, setting first value to comparable float
     for i in range(len(eye_tracking)):
         eye_tracking[i] = eye_tracking[i][:-1].split(",")
         eye_tracking[i][0] = float(eye_tracking[i][0]) / 1000
-    for i in range(len(emotion_data)):
+
+    # Parse emotiv data, setting first value to comparable float
+    emotion_data[0] = emotion_data[0][:-1].split(",")
+    emotion_data[0][0] = float(emotion_data[0][0])
+
+    for i in range(1, len(emotion_data)):
         emotion_data[i] = emotion_data[i][:-1].split(",")
         emotion_data[i][0] = float(emotion_data[i][0])
-    min_timestamp = min(eye_tracking[0][0], emotion_data[0][0])
+
+        # If emotiv data is empty, fill in rows with data from rows above
+        for j in range(1, len(emotion_data[i])):
+            if emotion_data[i][j].strip() == "":
+                emotion_data[i][j] = emotion_data[i - 1][j]
+
     i = 0
     for j in range(len(eye_tracking)):
         while i < len(emotion_data) and emotion_data[i][0] < eye_tracking[j][0]:
@@ -28,5 +40,3 @@ def synthesize_data(filename1, filename2):
     f3.write(emotion_header)
     f3.write(emotion_data)
     f3.close()
-
-synthesize_data("eyeTrackData.csv", "emotivData.csv")
